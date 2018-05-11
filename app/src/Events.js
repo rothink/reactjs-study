588b2b748bad35150cc93b0d76e4a4cb4c34c50c
Basis <rodrigoluz@live.com>
2018-05-11 18:30:24 -0300

    Event delete

--- app/src/Events.js
+++ app/src/Events.js
@@ -11,6 +11,7 @@ class Events extends Component {
 
         this.updateInputName = this.updateInputName.bind(this)
         this.clickEvent = this.clickEvent.bind(this)
+        this.deleteEvent = this.deleteEvent.bind(this)
     }
 
     updateInputName(e) {
@@ -26,12 +27,26 @@ class Events extends Component {
         })
     }
 
+    deleteEvent(item) {
+        const newState = this.state.data
+        if(newState.indexOf(item) > -1) {
+            newState.splice(newState.indexOf(item), 1)
+
+            this.setState({
+                data: newState
+            })
+        }
+    }
+
     render() {
 
         const { data } = this.state
 
         const itens = data.map((item) =>  {
-                return <li key={item}> {item.toString()} </li>
+                return  <li key={item}>
+                            {item.toString()} 
+                            <button type="button" onClick={ this.deleteEvent.bind(this, item) }>Delete </button>
+                        </li>
             })
         
 
