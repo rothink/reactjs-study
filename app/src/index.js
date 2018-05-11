4c01869c523b1e350f5de0604fe1237910cc9430
Basis <rodrigoluz@live.com>
2018-05-11 17:57:47 -0300

    Events

--- app/src/index.js
+++ app/src/index.js
@@ -6,13 +6,15 @@ import registerServiceWorker from './registerServiceWorker';
 
 import HelloWorld from './HelloWorld';
 import PropsValidation from './PropsValidation';
+import Events from './Events';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
 
 ReactDOM.render(
     // <HelloWorld txt = 'HelloWorld, eu sou o ReactJs!'/>,
-    <PropsValidation name="Rodrigo A." age='28' height="1.75" />,
+    // <PropsValidation name="Rodrigo A." age='28' height="1.75" />,
+    <Events />,
     document.getElementById("root")
 );
 
